63ff5b964f4faf799d749924de09487c0d5b15f8
unknown <grantgariepy@protonmail.com>
2021-04-27 11:48:07 -0600

    update test

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//comment describing how to add/remove code to server
+//git add .
+//git commit -m 'message'
+//git push
+//sign into server
+//navigate to repository dir 'var/ww/nitemare-bot
+//sudo git pull
+
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
